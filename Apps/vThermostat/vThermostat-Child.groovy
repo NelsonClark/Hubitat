146fc615d4f8b3345414612bfba9987444efd6a8
Nelson Clark <73347042+NelsonClark@users.noreply.github.com>
2020-11-03 20:58:20 -0500

    Update vThermostat-Child.groovy

--- Apps/vThermostat/vThermostat-Child.groovy
+++ Apps/vThermostat/vThermostat-Child.groovy
@@ -1,3 +1,21 @@
+/*
+ *  vThermostat Child App
+ *  Project URL: https://github.com/NelsonClark/Hubitat/tree/main/Apps/vThermostat
+ *  Copyright 2020 Nelson Clark
+ *
+ *  This app requires it's parent app and device driver to function, please go to the project page for more information.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ * 
+ */
+
 definition(
 	name: "vThermostat Child",
 	namespace: "nclark",
