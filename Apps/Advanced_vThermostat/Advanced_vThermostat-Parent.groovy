dc441887b8296d814f2dc6608fc7e19fdad57221
Nelson Clark <73347042+NelsonClark@users.noreply.github.com>
2020-11-10 22:05:44 -0500

    Update and rename Apps/vThermostat/vThermostat-Parent.groovy to Apps/Advanced_vThermostat/Advanced_vThermostat-Parent.groovy

--- Apps/Advanced_vThermostat/Advanced_vThermostat-Parent.groovy
+++ Apps/Advanced_vThermostat/Advanced_vThermostat-Parent.groovy
@@ -1,6 +1,6 @@
 /*
- *  vThermostat Parent App
- *  Project URL: https://github.com/NelsonClark/Hubitat/tree/main/Apps/vThermostat
+ *  Advanced vThermostat Parent App
+ *  Project URL: https://github.com/NelsonClark/Hubitat/tree/main/Apps/Advanced_vThermostat
  *  Copyright 2020 Nelson Clark
  *
  *  This app requires it's child app and device driver to function, please go to the project page for more information.
@@ -17,23 +17,23 @@
  */
 
 definition(
-	name: "vThermostat Manager",
+	name: "Advanced vThermostat Manager",
 	namespace: "nclark",
 	author: "Nelson Clark",
 	description: "Join any sensor(s) with any outlet(s) for virtual thermostat control.",
 	category: "Green Living",
 	iconUrl: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/vThermostat/vThermostat-logo-small.png",
 	iconX2Url: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/vThermostat/vThermostat-logo.png",
-	importUrl: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/vThermostat/vThermostat-Parent.groovy",
+	importUrl: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/Advanced_vThermostat/Advanced_vThermostat-Parent.groovy",
 	singleInstance: true
 )
 
 preferences {
-	page(name: "Install", title: "vThermostat Manager", install: true, uninstall: true) {
+	page(name: "Install", title: "Advanced vThermostat Manager", install: true, uninstall: true) {
 		section("Devices") {
 		}
 		section {
-			app(name: "thermostats", appName: "vThermostat Child", namespace: "nclark", title: "Add vThermostat", multiple: true)
+			app(name: "thermostats", appName: "Advanced vThermostat Child", namespace: "nclark", title: "Add vThermostat", multiple: true)
 		}
 	}
 }
